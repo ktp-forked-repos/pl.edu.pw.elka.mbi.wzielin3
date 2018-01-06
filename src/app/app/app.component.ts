d6de74e58531f7fb25662d61667cf286761b5f2f
wziel <zielinski.w.t@gmail.com>
2018-01-06 10:56:42 +0100

    Move simulation buttons from editor to app component

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -3,6 +3,8 @@ import {Simulator, SimulatorState} from '../model/simulator';
 import {SimulationDemoComponent} from '../simulation-demo/simulation-demo.component';
 import {SimulationParams} from '../model/simulation-params';
 import {CellFilledEvent, PathElementReconstructedEvent, SimulationFinishedEvent} from '../model/events';
+import {MatSnackBar} from '@angular/material';
+import {ErrorType} from '../model/error-type';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +13,26 @@ import {CellFilledEvent, PathElementReconstructedEvent, SimulationFinishedEvent}
 })
 export class AppComponent {
   simulator: Simulator = null;
-  simulationParams: SimulationParams;
+  inputSimulationParams: SimulationParams = new SimulationParams();
+  runningSimulationParams: SimulationParams;
   results: string[] = [];
   @ViewChild(SimulationDemoComponent) simulatorDemoComponent: SimulationDemoComponent;
 
+  constructor(public snackBarError: MatSnackBar) {
+
+  }
+
   startSimulation(event) {
-    this.simulationParams = event;
-    this.simulationParams.toUpperCase();
-    this.simulator = new Simulator(this.simulationParams);
-    this.simulatorDemoComponent.createCube(this.simulationParams);
+    if (this.inputSimulationParams.isEmptySequence()) {
+      this.snackBarError.open(ErrorType.Sequence, '', {duration: 5000});
+    } else if (this.inputSimulationParams.isEmptyValueInMatrix()) {
+      this.snackBarError.open(ErrorType.FitnessMatrix, '', {duration: 5000});
+    } else {
+      this.runningSimulationParams = this.inputSimulationParams.clone();
+      this.runningSimulationParams.toUpperCase();
+      this.simulator = new Simulator(this.runningSimulationParams);
+      this.simulatorDemoComponent.createCube(this.runningSimulationParams);
+    }
   }
 
   step() {
@@ -34,6 +47,9 @@ export class AppComponent {
     return eventStep;
   }
 
+  /**
+   * Skips all steps until simulation is finished.
+   */
   skipAllSteps() {
     while (!(this.simulator.step() instanceof SimulationFinishedEvent)) { }
     this.simulatorDemoComponent.putAllCellsValues();
@@ -41,6 +57,9 @@ export class AppComponent {
     this.showResult();
   }
 
+  /**
+   * Skips all steps while cube cells are being calculated.
+   */
   skipCalculatingCells() {
     while (this.simulator.getStatus() === SimulatorState.CalculatingCells) {
       this.simulator.step();
@@ -48,19 +67,32 @@ export class AppComponent {
     this.simulatorDemoComponent.putAllCellsValues();
     this.showResult();
   }
+  /**
+   * Returns true if simulation is running, false otherwise.
+   */
+  isSimulationRunning() {
+    return this.simulator !== null;
+  }
 
+  /**
+   * Returns true if simulation is running and finished.
+   */
   isSimulationFinished() {
-    if (this.simulator !== null) {
-      return this.simulator.getStatus() === SimulatorState.Finished;
-    }
-    return false;
+    return this.isSimulationRunning() && this.simulator.getStatus() === SimulatorState.Finished;
+  }
+
+  /**
+   * Returns true if simulation is running and all cube cells have been filled, false otherwise.
+   */
+  isCubeFilled() {
+    return this.isSimulationRunning() && this.simulator.getStatus() !== SimulatorState.CalculatingCells;
   }
 
   showResult() {
     this.results = [];
     const reconstructedPath = this.simulator.getReconstructedPath();
     for (let i = 0; i < reconstructedPath.length; ++i) {
-      for (let j = 0; j < this.simulationParams.sequences.length; ++j) {
+      for (let j = 0; j < this.runningSimulationParams.sequences.length; ++j) {
         if (i === 0) {
           this.results.push('');
         }
--- src/app/simulation-editor/simulation-editor.component.ts
+++ src/app/simulation-editor/simulation-editor.component.ts
@@ -2,76 +2,13 @@ import {Component, EventEmitter, Input, NgModule, Output} from '@angular/core';
 import {SimulationParams} from '../model/simulation-params';
 import {Simulator, SimulatorState} from '../model/simulator';
 import {MatSnackBar} from '@angular/material';
-import {ErrorType} from './input-error/error-type';
-
-
+import {ErrorType} from '../model/error-type';
 @Component({
   selector: 'app-simulation-editor',
   templateUrl: './simulation-editor.component.html',
   styleUrls: ['./simulation-editor.component.css']
 })
 export class SimulationEditorComponent {
-  simulationParams: SimulationParams = new SimulationParams();
-  @Input() simulator: Simulator;
-  @Output() onStartedSimulation: EventEmitter<SimulationParams> = new EventEmitter();
-  @Output() onStepCliked: EventEmitter<string> = new EventEmitter();
-  @Output() onSkipAllStepsClicked: EventEmitter<string> = new EventEmitter();
-  @Output() onSkipCalculatingCellsClicked: EventEmitter<string> = new EventEmitter();
-
-  constructor(public snackBarError: MatSnackBar) {
-  }
-
-  startSimulation() {
-    if (this.simulationParams.isEmptySequence()) {
-      this.snackBarError.open(ErrorType.Sequence, '', {duration: 5000});
-    } else if (this.simulationParams.isEmptyValueInMatrix()) {
-      this.snackBarError.open(ErrorType.FitnessMatrix, '', {duration: 5000});
-    } else {
-      this.onStartedSimulation.emit(this.simulationParams.clone());
-    }
-  }
-
-  /**
-   * Perform one simulation step.
-   *
-   * @return {AppEvent}
-   */
-  step() {
-    this.onStepCliked.emit();
-  }
-
-  /**
-   * Perform all simulation steps.
-   */
-  skipAllSteps() {
-    this.onSkipAllStepsClicked.emit();
-  }
-
-  /**
-   * Perform all steps until cube is filled.
-   */
-  skipCalculatingCells() {
-    this.onSkipCalculatingCellsClicked.emit();
-  }
-
-  /**
-   * Returns true if simulation is running, false otherwise.
-   */
-  isSimulationRunning() {
-    return this.simulator !== null;
-  }
-
-  /**
-   * Returns true if simulation is running and finished.
-   */
-  isSimulationFinished() {
-    return this.isSimulationRunning() && this.simulator.getStatus() === SimulatorState.Finished;;
-  }
-
-  /**
-   * Returns true if simulation is running and all cube cells have been filled, false otherwise.
-   */
-  isCubeFilled() {
-    return this.isSimulationRunning() && this.simulator.getStatus() !== SimulatorState.CalculatingCells;
-  }
+  @Output() @Input() simulationParams: SimulationParams;
+  constructor() { }
 }
